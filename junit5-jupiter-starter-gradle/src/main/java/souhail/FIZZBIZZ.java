cda63c18a96f365a36f87f54d4199d440bad69d8
SOUHAIL AOUZI <souhailaouzi1949@gmail.com>
2024-12-19 17:37:55 +0100

    Cycle 1: Implémenter la méthode think pour Buzz

--- junit5-jupiter-starter-gradle/src/main/java/souhail/FIZZBIZZ.java
+++ junit5-jupiter-starter-gradle/src/main/java/souhail/FIZZBIZZ.java
@@ -1,6 +1,14 @@
 package souhail;
 
 public class FIZZBIZZ {
-    
+    public static String think(int nbr) {
+        String output = "";
+        for(int i = 1; i <= nbr; i++) {
+            if(i % 5 == 0) {
+                output = output + "Buzz";
+            }
+        }
+        return output;
+    }
     }
   
