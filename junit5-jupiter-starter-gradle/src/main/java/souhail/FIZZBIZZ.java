ae3cb75df9d2e6a7cf47d5498aac0e243a86ad6d
SOUHAIL AOUZI <souhailaouzi1949@gmail.com>
2024-12-19 17:49:49 +0100

    Cycle 3: Implémenter la méthode think pour FizzBuzz

--- junit5-jupiter-starter-gradle/src/main/java/souhail/FIZZBIZZ.java
+++ junit5-jupiter-starter-gradle/src/main/java/souhail/FIZZBIZZ.java
@@ -4,15 +4,15 @@ public class FIZZBIZZ {
     public static String think(int nbr) {
         String output = "";
         for(int i = 1; i <= nbr; i++) {
-            if(i % 5 == 0) {
-                output = output + "Buzz";
-            }
-            else if(i % 3 == 0) {
+            if(i % 3 == 0 && i % 5 == 0) {
+                output = output + "FizzBuzz";
+            } else if(i % 3 == 0) {
                 output = output + "Fizz";
+            } else if(i % 5 == 0) {
+                output = output + "Buzz";
             }
         }
         return output;
     }
-}
 
-  
+}
